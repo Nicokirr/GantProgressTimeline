7ad05e08f17ab39a53fe60cd7665e5ae694b3fe6
Nicokirr <39644725+Nicokirr@users.noreply.github.com>
2019-09-22 18:32:47 +0200

    Add files via upload

---
+++ GantProgressTimeline/generated/ManifestTypes.d.ts
@@ -0,0 +1,10 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    dataSetGrid: ComponentFramework.PropertyTypes.DataSet;
+}
+export interface IOutputs {
+}
